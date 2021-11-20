899d64d48a306f9fa9cd3df80a129036f95bfaee
Ognjen Raketic <94393750+Rubystein@users.noreply.github.com>
2021-11-20 18:12:13 +0100

    "While_loops"

---
+++ Whileloops_exercise_1.rb
@@ -0,0 +1,22 @@
+def count_e(word)
+  count = 0
+
+  i = 0
+  while i < word.length
+    char = word[i]
+
+    if char == "e"
+
+      count += 1
+    end
+
+    i +=1
+    end
+
+    return count
+
+  end
+
+puts count_e("movie")
+puts count_e('excellent')
+
